a6a90ec9db85988eaf06f27b62a3fcf10fd9fb4b
unknown <wapkingrana7@gmail.com>
2021-02-05 15:58:43 -0800

    onek kisu siklam

---
+++ scope.js
@@ -0,0 +1,13 @@
+const bonus = 20;
+function(){
+    let result = first + second + bonus;
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "cranky";
+        //console.log(mood);
+    }
+    console.log(mood);
+    return result;
+}
