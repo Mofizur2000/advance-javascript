8368eb949787014ae66215df77597ea4c9b58dc8
unknown <wapkingrana7@gmail.com>
2021-02-05 00:56:17 -0800

    onek kisu siklam

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,16 @@
+const first = 2;
+const second = "2";
+// it compair only the valeue
+
+if(first == second){
+    console.log("conditon is true");
+}else{
+    console.log("condition is false");
+}
+
+// it compair the valeue and the type
+if(first === second){
+    console.log("conditon is true");
+}else{
+    console.log("condition is false");
+}
